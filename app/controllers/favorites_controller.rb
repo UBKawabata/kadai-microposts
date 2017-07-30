cbb09cee0b1978e5822e4c0cdd0376721208bb5a
Yuubi Kawabata <yuubi.kawabata.2011@gmail.com>
2017-07-30 08:46:50 +0000

    Favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,16 +2,16 @@ class FavoritesController < ApplicationController
   before_action :require_user_logged_in
 
   def create
-    micropost = Micropost.find(params[:favorite_id])
-    current_user.favorite(favorite)
+    micropost = Micropost.find(params[:micropost_id])
+    current_user.favorite(micropost)
     flash[:success] = '投稿をお気に入りに登録しました。'
-    redirect_to micropost
+    redirect_back(fallback_location: root_url)
   end
 
   def destroy
-    user = User.find(params[:follow_id])
-    current_user.unfavorite(favorite)
+    micropost = Micropost.find(params[:micropost_id])
+    current_user.unfavorite(micropost)
     flash[:success] = '投稿のお気に入りを解除しました。'
-    redirect_to micropost
+    redirect_back(fallback_location: root_url)
   end
 end
